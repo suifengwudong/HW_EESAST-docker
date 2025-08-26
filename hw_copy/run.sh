c41552de5371bf2f487c9f967996ee0156bbece9
suifengwudong <HCXCNC@outlook.com>
2025-08-26 10:32:09 +0800

    hw completed

--- hw_copy/run.sh
+++ hw_copy/run.sh
@@ -1,1 +1,5 @@
-# Add your commands here
+docker build -t hw_copy_img ./hw_copy
+docker run --it --rm -v %cd%/src:/app hw_copy_img bash
+add 1 2
+mul 3 4
+exit
--- hw_vol/run.sh
+++ hw_vol/run.sh
@@ -1,1 +1,7 @@
-# Add your commands here
+docker build -t hw_vol_img ./hw_vol
+docker run -it --rm -v %cd%/src:/work hw_vol_img bash
+cd /work
+g++ add.cpp -o add
+./add 5 6
+cp add /work/
+exit
